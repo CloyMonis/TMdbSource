d599633d7495f992cd8c0401788fbdd3bb99ca5d
Cloy.Monis <cloy.monis@ril.com>
2024-03-15 12:57:02 +0530

    Added Unit Test Case Coverage to 83 percent

--- Example/Tests/HttpClientTests.swift
+++ Example/Tests/HttpClientTests.swift
@@ -21,6 +21,7 @@ final class HttpClientTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         sut = nil
+        expectation = nil
     }
 
     func testWrongEndPoint() {
